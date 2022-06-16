1c22abd3a8d2d488163cd75aa4795129ee69809a
Esther Kuang <eghkuang@gmail.com>
2022-06-16 16:45:53 -0700

    Fix API key

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: '/client/src/index.jsx',
+    output: {
+        path: __dirname + '/client/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: './'
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader', 'eslint-loader'] // include eslint-loader
+        }]
+    }
+};
