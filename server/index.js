dcd436f2d36d60605dba954d33387753da122710
Esther Kuang <eghkuang@gmail.com>
2022-06-17 09:59:56 -0700

    axios requests work, need to figure out how to do individual axios request

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
-const controller = require('./controller.js');
+const axios = require("axios");
+const db = require('./model.js');
 
 const app = express();
 const PORT = 4000;
@@ -8,39 +8,47 @@ const PORT = 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.get('/api/allPlants', (req, res) => {
     //TODO - your code here!
     console.log('api/allPlants?');
-    controller.getAllPlants((err, results) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.status(200).send(results);
-        }
-    });
+    // db.allPlants((err, results) => {
+    //     console.log('plant data?', results.data);
+    //     if (err) {
+    //         res.status(500).send(err);
+    //     } else {
+    //         res.status(200).send(results.data);
+    //     }
+    // });
+    axios.request(db.allPlants)
+        .then(function (response) {
+            console.log(response.data);
+            res.status(200).send(response.data);
+        })
+        .catch(function (error) {
+            console.error(error);
+            res.status(404).send(error);
+        });
 });
 
-app.get('/api/card', (req, res) => {
+app.get('/api/onePlant', (req, res) => {
     //TODO - your code here!
-    controller.getOneCard(req.query, (err, results) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.status(200).send(results);
-        }
-    });
-});
-
-app.patch('/api/phrases', (req, res) => {
-    console.log('patch reqbody', req.body);
-    controller.updatePhrase(req.body, (err, results) => {
-        if (err) {
-            console.log('patch err', err);
-            res.status(500).send(err);
-        } else {
-            res.status(202).send(results);
-        }
-    });
+    // db.onePlant(req.query, (err, results) => {
+    //     if (err) {
+    //         res.status(500).send(err);
+    //     } else {
+    //         res.status(200).send(results);
+    //     }
+    // });
+    axios.request(db.onePlant)
+        .then(function (response) {
+            console.log(response.data);
+            res.status(200).send(response.data);
+        })
+        .catch(function (error) {
+            console.error(error);
+            res.status(404).send(error);
+        });
 });
 
 //TODO - add additional route handlers as necessary
--- server/model.js
+++ server/model.js
@@ -1,1 +1,19 @@
-const db = require('./index.js');
+const allPlants = {
+    method: 'GET',
+    url: 'https://house-plants.p.rapidapi.com/all',
+    headers: {
+        'X-RapidAPI-Key': '92281c861amsh0dc7479d29c87dbp1abf0fjsn4f04df345ae3',
+        'X-RapidAPI-Host': 'house-plants.p.rapidapi.com'
+    }
+};
+
+const onePlant = {
+    method: 'GET',
+    url: 'https://house-plants.p.rapidapi.com/common/silvervase',
+    headers: {
+        'X-RapidAPI-Key': '92281c861amsh0dc7479d29c87dbp1abf0fjsn4f04df345ae3',
+        'X-RapidAPI-Host': 'house-plants.p.rapidapi.com'
+    }
+};
+
+module.exports = { allPlants, onePlant };
