fb9c62f778bbdbc6f87a39a6498a515926fd5026
Esther Kuang <eghkuang@gmail.com>
2022-06-17 11:39:10 -0700

    added key and host to gitignore

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,49 @@
+import axios from 'axios';
 import React from 'react';
-// import axios from 'axios';
-
+import styled from 'styled-components';
 class App extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            allPlants: [],
+            currentPlant: {}
+        };
+    }
+
+    componentDidMount () {
+        this.allPlants();
+    }
+
+    allPlants () {
+        axios.get('/api/allPlants')
+            .then((res) => {
+                console.log('res?', res);
+                this.setState({
+                    plants: res.data
+                });
+            });
+    }
+
+    onePlant () {
+        axios.get('/api/onePlant')
+            .then((res) => {
+                console.log('res?', res);
+                this.setState({
+                    currentPlant: res.data
+                });
+            });
+    }
+
     render () {
         return (
-            <h1>plant stuuuuff</h1>
+            <Title>plant stuuuuff</Title>
         );
     }
 }
+
+const Title = styled.h1`
+  margin-left: 20px;
+  color: #D8315B;
+`;
 
 export default App;
--- server/model.js
+++ server/model.js
@@ -1,9 +1,11 @@
+const { key, host } = require('../config.js');
+
 const allPlants = {
     method: 'GET',
     url: 'https://house-plants.p.rapidapi.com/all',
     headers: {
-        'X-RapidAPI-Key': '92281c861amsh0dc7479d29c87dbp1abf0fjsn4f04df345ae3',
-        'X-RapidAPI-Host': 'house-plants.p.rapidapi.com'
+        'X-RapidAPI-Key': key,
+        'X-RapidAPI-Host': host
     }
 };
 
@@ -11,8 +13,8 @@ const onePlant = {
     method: 'GET',
     url: 'https://house-plants.p.rapidapi.com/common/silvervase',
     headers: {
-        'X-RapidAPI-Key': '92281c861amsh0dc7479d29c87dbp1abf0fjsn4f04df345ae3',
-        'X-RapidAPI-Host': 'house-plants.p.rapidapi.com'
+        'X-RapidAPI-Key': key,
+        'X-RapidAPI-Host': host
     }
 };
 
